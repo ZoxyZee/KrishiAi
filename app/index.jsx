1923d9e9770a6747ba385167cae4f786977c45a9
Vishal Kumar <146015120+vishal-kr-dev@users.noreply.github.com>
2025-09-11 10:16:05 +0530

    Demo

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,15 @@
-import { Text, View } from "react-native";
+import { View, Text, Button } from "react-native";
+import { useRouter } from "expo-router";
 
-export default function Index() {
+export default function Home() {
+  const router = useRouter();
+
   return (
-    <View
-    className="bg-red-600"
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{ flex:1, justifyContent:"center", alignItems:"center" }}>
+      <Text style={{ fontSize:24, marginBottom:20 }}>Hello World 👋</Text>
+      <Button title="Go to Login" onPress={() => router.push("/login")} />
+      <Button title="Go to About" onPress={() => router.push("/about")} />
+      <Button title="Go to Profile" onPress={() => router.push("/profile")} />
     </View>
   );
 }
